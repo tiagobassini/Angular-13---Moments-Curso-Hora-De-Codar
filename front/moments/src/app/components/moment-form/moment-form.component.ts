fd74cdf0c4ad603591dbc11ef0f0fcd15045e965
tiagobassini <tiagobassini@gmail.com>
2023-01-10 12:23:33 -0300

    aula 29

---
+++ front/moments/src/app/components/moment-form/moment-form.component.ts
@@ -0,0 +1,58 @@
+import { outputAst } from '@angular/compiler';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+import { FormGroup, FormControl, Validators} from '@angular/forms'
+import { Moment } from 'src/app/Moment';
+
+@Component({
+  selector: 'app-moment-form',
+  templateUrl: './moment-form.component.html',
+  styleUrls: ['./moment-form.component.css']
+})
+export class MomentFormComponent {
+
+  @Output()
+  onSubmit = new EventEmitter<Moment>()
+
+
+  @Input()
+  btnText!: string;
+
+  momentForm!:FormGroup;
+  
+
+  ngOnInit():void{
+    this.momentForm = new FormGroup({
+      id: new FormControl(''),
+      title: new FormControl('',[Validators.required]),
+      description: new FormControl('',[Validators.required]),
+      image: new FormControl('')
+    });
+  }
+
+  get title(){
+    return this.momentForm.get('title')!;
+  }
+  get description(){
+    return this.momentForm.get('description')!;
+  }
+
+  
+  onFileSelected(event: any){
+    const file: File = event.target.files[0];
+
+    this.momentForm.patchValue({image: file})
+  }
+  
+  submit(){
+
+    if(this.momentForm.invalid){
+      return;
+    }
+    console.log(this.momentForm.value);
+
+    this.onSubmit.emit(this.momentForm.value);
+  }
+
+
+}
--- front/moments/src/app/components/pages/new-moment/new-moment.component.ts
+++ front/moments/src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Moment } from 'src/app/Moment';
+import { MomentService } from 'src/app/services/moment.service';
 
 @Component({
   selector: 'app-new-moment',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./new-moment.component.css']
 })
 export class NewMomentComponent {
+
+  btnText:string = "Compartilhar!";
+
+
+  constructor(private momentService: MomentService){ }
+
+  async createHandler(moment:Moment){
+
+    const formData = new FormData();
+
+    formData.append('title', moment.title);
+    formData.append('description', moment.description);
+
+    if (moment.image){
+      formData.append('image', moment.image);
+    }
+
+    await this.momentService.createMoment(formData).subscribe();
+
+  }
+
 
 }
